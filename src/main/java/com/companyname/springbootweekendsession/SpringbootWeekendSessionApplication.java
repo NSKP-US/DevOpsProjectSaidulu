9766c0784b29d101be407e20dabea03cb7218c29
nskp <nskp.us@gmail.com>
2021-01-05 13:18:41 +0530

    FirstCommit

---
+++ src/main/java/com/companyname/springbootweekendsession/SpringbootWeekendSessionApplication.java
@@ -0,0 +1,16 @@
+package com.companyname.springbootweekendsession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages= {"com.companyname.controller"})
+public class SpringbootWeekendSessionApplication {
+	// test
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWeekendSessionApplication.class, args);
+	}
+
+}
